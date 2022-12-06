3c393d8820b26cae70195c3a794cef5030791e29
Eduariag <uariagsmart@gmail.com>
2022-12-06 21:51:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Из трехзначного числа убрать среднюю и оставшиеся вывести на экран
+/*
+int CutNumber (int num)
+{
+    int ed= num %10;
+    int sot=num /100;
+
+    int result= sot*10 +  ed;
+    return result;
+}
+
+int RandNum= new Random().Next(100,1000);
+int newNum=CutNumber(RandNum);
+
+Console.WriteLine($" number {RandNum} is {newNum}");
+*/
+
+
+
