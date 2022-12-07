dedf29ebea5a7877232ae3eb89c52d4b44f10cb0
Eduariag <uariagsmart@gmail.com>
2022-12-07 18:03:23 +0300

    2-я задача

--- Program.cs
+++ Program.cs
@@ -1,19 +1,30 @@
-﻿// Из трехзначного числа убрать среднюю и оставшиеся вывести на экран
+﻿ // Из трехзначного числа убрать среднюю и оставшиеся вывести на экран
+
 /*
 int CutNumber (int num)
 {
     int ed= num %10;
     int sot=num /100;
-
     int result= sot*10 +  ed;
     return result;
 }
-
 int RandNum= new Random().Next(100,1000);
 int newNum=CutNumber(RandNum);
-
 Console.WriteLine($" number {RandNum} is {newNum}");
 */
 
+// Показать случайное двухзначное число и показать большую цифру
 
+Int CutNumber(int num)
+{
+    int ed=num%10;
+    int ten=num/10;
+
+    ten=max;
+    if(max>ed);
+    return max;
+}
+intRandNum=new Random().Next(10,100);
+int NewNum=CutNumber(RandNum);
+Console.WriteLine($" number {RandNum} is {newNum}");
 
